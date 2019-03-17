8b36227377e2b8aeb6e834fe34f66560de46c62b
d-guo <dguo@ucsb.edu>
2019-03-17 01:59:52 -0700

    started working on random generation of a network but brain is tired, need to fix parameter values for optimizer

--- evol.py
+++ evol.py
@@ -2,3 +2,68 @@ import random
 import nn
 
 #create a NN with random parameters
+def create_NN():
+	#pick number of layers
+	num_layers = random.choice([2, 3, 6, 9])
+
+	#pick activation function
+	actf_id = random.randint(0, 10)
+	if(actf_id  == 0):
+		actf = nn.nn.ELU()
+	if(actf_id  == 1):
+		actf = nn.nn.Hardshrink()
+	if(actf_id  == 2):
+		actf = nn.nn.LeakyReLU()
+	if(actf_id  == 3):
+		actf = nn.nn.LogSigmoid()
+	if(actf_id  == 4):
+		actf = nn.nn.PReLU()
+	if(actf_id  == 5):
+		actf = nn.nn.ReLU()
+	if(actf_id  == 6):
+		actf = nn.nn.ReLU6()
+	if(actf_id  == 7):
+		actf = nn.nn.RReLU()
+	if(actf_id  == 8):
+		actf = nn.nn.SELU()
+	if(actf_id  == 9):
+		actf = nn.nn.CELU()
+
+	#create neural network
+	net = nn.NN(num_layers, actf)
+
+	#pick random optimizer
+	optim_id = random.randint(0, 10)
+	if(optim_id  == 0):
+		optim = nn.torch.optim.Adadelta(net.parameters())
+		net.optimizer = optim
+	if(optim_id  == 1):
+		optim = nn.torch.optim.Adagrad(net.parameters())
+		net.optimizer = optim
+	if(optim_id  == 2):
+		optim = nn.torch.optim.Adam(net.parameters())
+		net.optimizer = optim
+	if(optim_id  == 3):
+		optim = nn.torch.optim.SparseAdam(net.parameters())
+		net.optimizer = optim
+	if(optim_id  == 4):
+		optim = nn.torch.optim.Adamax(net.parameters())
+		net.optimizer = optim
+	if(optim_id  == 5):
+		optim = nn.torch.optim.ASGD(net.parameters())
+		net.optimizer = optim
+	if(optim_id  == 6):
+		optim = nn.torch.optim.LBFGS(net.parameters())
+		net.optimizer = optim
+	if(optim_id  == 7):
+		optim = nn.torch.optim.RMSprop(net.parameters())
+		net.optimizer = optim
+	if(optim_id  == 8):
+		optim = nn.torch.optim.Rprop(net.parameters())
+		net.optimizer = optim
+	if(optim_id  == 9):
+		optim = nn.torch.optim.SGD(net.parameters())
+		net.optimizer = optim
+	return net
+
+foo = create_NN()
--- nn.py
+++ nn.py
@@ -2,58 +2,60 @@
 import torch
 import torch.nn as nn
 
-#create a NN with the specified parameters in param
-#params[0] = number of layers
-#params[1] = optimizer
-#params[2] = activator
-def create_NN(params):
-	return NN(params[0], params[1], params[2], params)
-
 class NN(nn.Module):
-	def __init__(self, num_layers, optimizer, activator, params):
-		super(ConvNet, self).__init__()
-
-		#keep track of own params
-		self.params = params
-
-		#set optimizer
-		#can be one of the following: Adadelta, Adagrad, Adam, SparseAdam, Adamax, ASGD, LBFGS, RMSprop, Rprop, SGD
-		self.optimizer = optimizer
+	def __init__(self, num_layers, activator):
+		super(NN, self).__init__()
 
 		#set activation function
-		#can be one of the following: ELU, Hardshrink, Hardtanh, LeakyReLU, LogSigmoid, PReLU, ReLUm ReLU6, RReLU, SELU, CELU, Sigmoid, Softplus, Softshrink, Softsign, Tanh, TanhShrink, Threshold
+		#can be one of the following: ELU, Hardshrink, LeakyReLU, LogSigmoid, PReLU, ReLU, ReLU6, RReLU, SELU, CELU, Sigmoid
 		self.activator = activator
 
 		#store layers in list
-		self.layers = list()
+		self.layers = nn.ParameterList()
 
 		#we want to input a 28x28 images (784 values) and return an integer 0-9
 		#to do this, we take 784 - 10 = 774 which factors into 2, 3, 6, 9
 		#to make layer splitting easier, the only options for num_layers will be limited to these numbers
 		if(num_layers == 2):	#774 / 2 = 387
-			self.layers.append(nn.linear(784, 397))
-			self.layers.append(nn.linear(397, 10))
+			self.layers.append(nn.Linear(784, 397))
+			self.layers.append(self.activator)
+			self.layers.append(nn.Linear(397, 10))
 		if(num_layers == 3):	#774 / 3 = 258
-			self.layers.append(nn.linear(784, 526))
-			self.layers.append(nn.linear(526, 268))
-			self.layers.append(nn.linear(268, 10))
+			self.layers.append(nn.Linear(784, 526))
+			self.layers.append(self.activator)
+			self.layers.append(nn.Linear(526, 268))
+			self.layers.append(self.activator)
+			self.layers.append(nn.Linear(268, 10))
 		if(num_layers == 6):	#774 / 6 = 129
-			self.layers.append(nn.linear(784, 655))
-			self.layers.append(nn.linear(655, 526))
-			self.layers.append(nn.linear(526, 396))
-			self.layers.append(nn.linear(396, 268))
-			self.layers.append(nn.linear(268, 139))
-			self.layers.append(nn.linear(139, 10))
+			self.layers.append(nn.Linear(784, 655))
+			self.layers.append(self.activator)
+			self.layers.append(nn.Linear(655, 526))
+			self.layers.append(self.activator)
+			self.layers.append(nn.Linear(526, 396))
+			self.layers.append(self.activator)
+			self.layers.append(nn.Linear(396, 268))
+			self.layers.append(self.activator)
+			self.layers.append(nn.Linear(268, 139))
+			self.layers.append(self.activator)
+			self.layers.append(nn.Linear(139, 10))
 		if(num_layers == 9):	#774 / 9 = 86
-			self.layers.append(nn.linear(784, 698))
-			self.layers.append(nn.linear(698, 612))
-			self.layers.append(nn.linear(612, 526))
-			self.layers.append(nn.linear(526, 440))
-			self.layers.append(nn.linear(440, 354))
-			self.layers.append(nn.linear(354, 268))
-			self.layers.append(nn.linear(268, 182))
-			self.layers.append(nn.linear(182, 96))
-			self.layers.append(nn.linear(96, 10))
+			self.layers.append(nn.Linear(784, 698))
+			self.layers.append(self.activator)
+			self.layers.append(nn.Linear(698, 612))
+			self.layers.append(self.activator)
+			self.layers.append(nn.Linear(612, 526))
+			self.layers.append(self.activator)
+			self.layers.append(nn.Linear(526, 440))
+			self.layers.append(self.activator)
+			self.layers.append(nn.Linear(440, 354))
+			self.layers.append(self.activator)
+			self.layers.append(nn.Linear(354, 268))
+			self.layers.append(self.activator)
+			self.layers.append(nn.Linear(268, 182))
+			self.layers.append(self.activator)
+			self.layers.append(nn.Linear(182, 96))
+			self.layers.append(self.activator)
+			self.layers.append(nn.Linear(96, 10))
 
 	def forward(self, x):
 		for layer in self.layers:
